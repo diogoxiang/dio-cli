48b2eab5ea42244b1cc0b4574f39913b8eaa0872
a233894432 <a233894432@gmail.com>
2017-05-16 14:57:43 +0800

    1\ 增加 omi 模板

--- bin/dio-cli.js
+++ bin/dio-cli.js
@@ -23,6 +23,18 @@ program
     .on('-h, --help', help);
 
 
+
+program
+    .command('init [projectName]')
+    .description('Initialize a new Dio application in the current folder')
+    .action(function(projectName, option) {
+        var cmd = 'init';
+        if (option.parent.mirror) {
+            options.mirror = option.parent.mirror;
+        }
+        switchCommand(cmd, { project: projectName, mirror: options.mirror, language: options.language })
+    })
+
 program.parse(process.argv);
 
 // let pname = program.args[0]
@@ -45,4 +57,18 @@ function switchCommand(cmd, args) {
     } else {
         setTimeout(program.help, 0);
     }
+}
+
+/**
+ * 
+ * @func help method
+ * 
+ */
+function help() {
+    console.log('  Commands:');
+    console.log();
+    console.log(`     ${chalk.green('init  [project-name]')}           Initialize a new Dio application in the current folder `);
+    console.log(`     ${chalk.green('pr')}                             Compile your Dio project `)
+    console.log();
+    console.log('  All commands can be run with -h (or --help) for more information.')
 }
---
+++ templates/app/src/js/config.js
@@ -0,0 +1,11 @@
+import config from '../../project.js';
+
+window.Root = window.Root||{ };
+
+Root.CDN = config.cdn;
+
+if(!Root.hasOwnProperty('isDev')) {
+    Root.isDev = true;
+}
+
+export default  Root;
