0e672a850fe93c04357bb6810216989ca487a48d
Diogoxiang <a233894432@gmail.com>
2020-05-20 14:03:17 +0800

    优化一个 报错的问题

--- bin/dio.js
+++ bin/dio.js
@@ -10,33 +10,33 @@ const checkVersion = require("../lib/check-version");
 const { resolveCwd } = require("../lib/utils");
 
 main();
- 
-async function main() {
-  if (process.argv[2] !== "lint" && process.argv[2] !== "bug") await beforeInit();
-  // 注入  Command 参数
-  program
-    .version(require("../package").version)
-    .usage("<command> [options]")
-    .command("init", "创建一个基于Dio的项目")
-    .command("server", "运行开发服务")
-    .command("build", "打包项目")
-    .command("update", "更新框架以及命令行工具至最新版本")
-    .command("lint", "校验 JS 代码 并修复一些JS错误")
-    .command("bug", "debug 开发代码")
-    .parse(process.argv);
 
-  registerLogger("", process);
+async function main() {
+	// if (process.argv[2] !== "lint" && process.argv[2] !== "bug") await beforeInit();
+	// 注入  Command 参数
+	program
+		.version(require("../package").version)
+		.usage("<command> [options]")
+		.command("init", "创建一个基于Dio的项目")
+		.command("server", "运行开发服务")
+		.command("build", "打包项目")
+		.command("update", "更新框架以及命令行工具至最新版本")
+		.command("lint", "校验 JS 代码 并修复一些JS错误")
+		.command("bug", "debug 开发代码")
+		.parse(process.argv);
+
+	registerLogger("", process);
 }
 
 // 初始化前.进行版本检测  UI检测 .及NODE检测
 async function beforeInit() {
-  if (process.argv[2] !== "init") {
-    checkHasInited();
+	if (process.argv[2] !== "init") {
+		checkHasInited();
 
-    // 检测dio-ui UI库
-    const packageJson = require(resolveCwd("package.json"));
-    if (packageJson.dependencies && packageJson.dependencies["dio-ui"])
-      await checkVersion("dio-ui")();
-  }
-  await checkVersion("dio-cli")();
+		// 检测dio-ui UI库
+		const packageJson = require(resolveCwd("package.json"));
+		if (packageJson.dependencies && packageJson.dependencies["dio-ui"])
+			await checkVersion("dio-ui")();
+	}
+	await checkVersion("dio-cli")();
 }
